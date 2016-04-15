6ac03f06e57609805eff653b7b3f611bdae2a9fb
Peter Lycksén <peter.lycksen@cirrato.com>
2016-04-15 08:32:08 +0200

    .

---
+++ public/scripts/MyChart.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+	var ctx = document.getElementById("myChart").getContext("2d");
+
+
+var options = {
+	animationSteps: 200,
+	bezierCurve : false,
+}
+
+var data = {
+    labels: ["5min", "10min", "15min", "20min", "25min", "30min", "35min"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 34, 59, 80, 81, 56, 55, 40,55]
+        }
+       /* {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }*/
+    ]
+};
+	var myNewChart = new Chart(ctx).Line(data, options);
+
+
+	/*setTimeout(function(){ alert("sdf"); }, 1000);*/
+}
